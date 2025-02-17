40ba52a6d08f06c70c61c7fe0656d8bf6cc215ac
andrew <andrew@FALINKS>
2025-02-17 12:13:40 -0500

    Initial commit

---
+++ src/FortuneTellerFrame.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+
+public class FortuneTellerFrame extends JFrame{
+
+
+
+    public FortuneTellerFrame(){
+        setTitle("Fortune Teller");
+        setSize(400,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        //Top Panel
+        /*
+        A label with "Fortune Teller"
+        An image icon "magic orb"
+        Large font size 36-48
+        add image file to directory
+         */
+
+
+
+        //Middle Panel
+        /*
+        JScroll panel{
+        JText area
+        "Fortunes displayed here"
+        }
+         */
+
+
+
+
+        //Bottom Panel
+        /*
+        Button "Read my Fortune"
+        Button "Quit"
+         */
+
+    }
+
+}
---
+++ src/FortuneTellerViewer.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class FortuneTellerViewer {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new FortuneTellerFrame();
+        ArrayList<String> fortuneList = new ArrayList<>();
+
+        //Add 12 hilarious fortunes here
+        fortuneList.add();
+
+
+
+
+
+
+    }
+}
